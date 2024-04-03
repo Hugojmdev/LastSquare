700a5e648bff9bea9c5c548adafba3040926549e
Hugo Jimenez <hgojm.dev@gmail.com>
2024-04-02 21:50:16 -0400

    Fixed square spawner to avoid spawn loop

--- Assets/Src/Controllers/Helpers/SquareCtrl.cs
+++ Assets/Src/Controllers/Helpers/SquareCtrl.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class SquareCtrl : MonoBehaviour {
@@ -26,33 +27,38 @@ public class SquareCtrl : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        //Will spawn a 'square' if it's available to do so
-        Spawn();
+        //Will spawn squares
+        Spawner();
+    }
+
+    //Will spawn a square depending on which button is pressed and square cost.
+    private void Spawner(){
+        //Gets amount of squares from player data.
+        int squares = (int)playerDataMgr.GetPlayer().squares;
+        //Spawns 'groundSquare' if press 'J' and there are squares available, cost=1 square
+        if (Input.GetKeyDown(KeyCode.J) && squares >= 1) Spawn(groundSquare);
+        //Spawns 'damageSquare' if press 'K' and there are squares available, cost=1 square
+        if (Input.GetKeyDown(KeyCode.K) && squares >= 1) Spawn(damageSquare);
+        //Spawns 'explosiveSquare' if press 'L' and there are squares available, cost=2 squares
+        if (Input.GetKeyDown(KeyCode.L) && squares >= 2) Spawn(explosiveSquare);
     }
 
     //Spawns a square depending on what key was pressed down and square cost
-     private void Spawn(){
-        //Gets available amount of squares
-        int squares = (int)playerDataMgr.GetPlayer().squares;
-        //Initialize square prefab.
-        GameObject squarePrefab = new("NoSquare");
-        //Spawns 'groundSquare' if press 'J' and there are squares available, cost=1 square
-        if(Input.GetKeyDown(KeyCode.J) && squares >= 1) squarePrefab = groundSquare;
-        //Spawns 'damageSquare' if press 'K' and there are squares available, cost=1 square 
-        if(Input.GetKeyDown(KeyCode.K) && squares >= 1) squarePrefab = damageSquare;
-        //Spawns 'explosiveSquare' if press 'L' and there are squares available, cost=2 squares
-        if(Input.GetKeyDown(KeyCode.L) && squares >= 2) squarePrefab = explosiveSquare;
-        //Verify if the give target position is available to spawn and square prefab is an actual square.
+     private void Spawn(GameObject squarePrefab){
+        //GameObject squarePrefab = GetSquarePrefab();
         if(IsTargetPositionAvailable() && Tag.SQUARE_TAGS.Contains(squarePrefab.tag)) {
             //Get spawn position.
             Vector3 playerPosition = transform.position;
-            Vector3 spawnPosition = new Vector3(playerPosition.x, playerPosition.y -0.5f, playerPosition.z); 
-            //Instantiates a new consumable object to be spawned.        
+            Vector3 spawnPosition = new(playerPosition.x, playerPosition.y -0.5f, playerPosition.z); 
+            //Instantiates a new consumable object to be spawned with the required components        
             GameObject square = Instantiate(squarePrefab, spawnPosition, Quaternion.identity);
             square.name = squarePrefab.name;
             square.transform.SetParent(GameObject.Find(LevelObjects.SQUARES).transform);
-            playerDataMgr.UpdateSquares(-1);
+            //Update squares amount depending on square type spawned
+            if (squarePrefab.CompareTag(Tag.GROUND_SQUARE) || squarePrefab.CompareTag(Tag.DAMAGE_SQUARE)) playerDataMgr.UpdateSquares(-1);
+            if (squarePrefab.CompareTag(Tag.EXPLOSIVE_SQUARE)) playerDataMgr.UpdateSquares(-2);
         }
+        
     }
 
     //This will verify if target position is available to spawn.
